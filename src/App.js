71358275b51e5a80b70c8bb3c27fd816f5654d51
AkeelLegend <akeelyahoo@gmail.com>
2021-05-31 11:37:06 +0530

    logo.svg import removed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, {useState, useRef, useEffect} from 'react';
 import Form from "./components/UsernameForm";
 import Chat from "./components/Chat";
 import io from "socket.io-client";
-import immer from "immer"
-import logo from './logo.svg';
+import immer from "immer";
 import './App.css';
 
 const initialMessageState = {
